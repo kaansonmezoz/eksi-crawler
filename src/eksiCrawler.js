5232127781754a4cb0b9b8830c6a25eb77a3e1c1
kaansonmezoz <ksonmezoz@outlook.com>
2019-09-22 18:09:21 +0300

    Mkdir added

--- src/eksiCrawler.js
+++ src/eksiCrawler.js
@@ -5,7 +5,13 @@ const {getUserEntries} = require('./getAllUserEntries');
 getUserEntries(username).then((entries) => {
     let jsonString = JSON.stringify(entries, null, 2);
     console.log('Entries has been writing to the file !');
-    fs.writeFileSync(`${username}.json`, jsonString,'utf8');
+    
+    if(!fs.existsSync('../entries')){
+        fs.mkdirSync('../entries');
+    }
+
+    fs.writeFileSync(`../entries/${username}.json`, jsonString,'utf8');
+    
     console.log('Entries has been written to the file !');
 })
 .catch((err) => {
